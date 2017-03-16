1dcf60df174bbd7d546e0fc8938a86ed7ed47d8a
faldyif <faldy.if@gmail.com>
2017-03-16 20:26:38 +0700

    Pertemuan pertama

---
+++ app/src/main/java/id/ac/ugm/sv/himakomsi/sischat/Chat.java
@@ -0,0 +1,17 @@
+package id.ac.ugm.sv.himakomsi.sischat;
+
+import java.util.Date;
+
+/**
+ * Created by Faldy on 16/03/2017.
+ */
+
+public class Chat {
+    String nama;
+    String pesan;
+    Date waktuKirim;
+
+    public void send() {
+
+    }
+}
